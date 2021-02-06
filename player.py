0682fa1c66961de32a519c55574cb98385a6ffc9
af2111 <af@martinfritz.net>
2021-02-06 22:36:35 +0100

    added stop functionality to the only play one song action

--- player.py
+++ player.py
@@ -23,19 +23,28 @@ What do you want to do?
 (3) Play all songs
 """)
 if action == "1":
+    #take query from user
     searchquery = input("input a query\n")
     results = search(searchquery)
     clear()
+    #print every results title
     for i in range(len(results)):
         print(f"({i + 1}): {results[i]['title']}")
+    #take one song and download it
     num = int(input("which one do you want to download?\n")) - 1
     downloadMP3(idToYtURL(results[num]["id"]))
 elif action == "2":
+    #get all files in the music folder
     songs = listdir(config.mp3_dest)
+    #print all the names without the extension
     for i in range(len(songs)):
-        print(f"({i + 1}): {songs[i]}")
+        print(f"({i + 1}): {songs[i].split('.mp3')[0]}")
     num = int(input("which one do you want to play?\n")) - 1
-    playsound(f"{config.mp3_dest}/{songs[num]}")
+    #play the users chosen song as a thread
+    p = multiprocessing.Process(target=playsound, args=(f"{config.mp3_dest}/{songs[num]}", ))
+    p.start()
+    input("press enter to stop the song")
+    p.terminate()
 elif action == "3":
     for song in listdir(config.mp3_dest):
         p = multiprocessing.Process(target=playsound, args=(f"{config.mp3_dest}/{song}", ))
